66585a8bc2e9e2a4a14d3453f304aec0cd8d9feb
Jack Dohrman <jdohrman@clarkenersen.com>
2026-05-29 12:05:12 -0500

    Fix GitHub Pages AI status check

--- src/components/StakeholderMap.jsx
+++ src/components/StakeholderMap.jsx
@@ -18220,11 +18220,6 @@ const StakeholderMap = ({
 
   useEffect(() => {
     const aiBase = getAiBaseUrl();
-    const isStaticHost = isStaticGithubHost();
-    if (isStaticHost) {
-      setAiStatus('down');
-      return () => {};
-    }
     if (!aiBase) {
       setAiStatus('down');
       return () => {};
